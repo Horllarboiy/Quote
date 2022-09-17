8eab7588bb0b98f58ba1df4dd571b2c43eae98d8
Horllarboiy <46043291+Horllarboiy@users.noreply.github.com>
2022-09-17 20:18:42 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Start editing t
+        o see some magic happen :)</p>
     </div>
   );
 }
